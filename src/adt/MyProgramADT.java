98ffd0d47af831d7493896d866c898a4351c9b0d
bgautam9045 <bharti.gautam_cs18>
2020-02-23 23:05:50 +0530

    implementing view() method to display all the contacts in the contact list

--- src/adt/MyProgramADT.java
+++ src/adt/MyProgramADT.java
@@ -5,5 +5,6 @@ import defination.Person;
 public interface MyProgramADT {
     //add() method to add an contact to the contact list
     void add(Person item);
-
+    //view() method to display all the contacts in the contact list
+    void view();
 }
