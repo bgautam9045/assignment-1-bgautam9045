4c132d8c8fc3eb175dcef4f29f3261f88fde1278
bgautam9045 <bharti.gautam_cs18>
2020-02-23 22:33:12 +0530

    constructor to intialise both fields of node

--- src/defination/Node.java
+++ src/defination/Node.java
@@ -1,4 +1,13 @@
 package defination;
 
 public class Node {
+    // these are the fields for the contact list
+    private Person data;
+    private Node next;
+
+    // constructor to initialize both the fields of the node
+    Node(Person data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
--- src/defination/Person.java
+++ src/defination/Person.java
@@ -1,4 +1,29 @@
 package defination;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Person {
+    // these are the fields for the person class
+    private String FName;
+    private String LName;
+    private ArrayList<BigInteger> contactList = new ArrayList<>();
+    private String email;
+
+    //getter methods for the fields of person class
+    public String getFName() {
+        return FName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<BigInteger> getContactList() {
+        return contactList;
+    }
 }
