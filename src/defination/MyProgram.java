a81eb087eb6855c589c1259f10347f3fcd853fc9
bgautam9045 <bharti.gautam_cs18>
2020-02-23 23:11:34 +0530

    view() method to view the complete contact list.

--- src/defination/MyProgram.java
+++ src/defination/MyProgram.java
@@ -2,7 +2,10 @@ package defination;
 
 import adt.MyProgramADT;
 
-public class MyProgram implements MyProgramADT, defination.MyProgramADT {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MyProgram implements MyProgramADT{
     // This reference variable will store the reference to the first contact of the contact list.
     private Node head = null;
 
@@ -14,5 +17,41 @@ public class MyProgram implements MyProgramADT, defination.MyProgramADT {
         Node node = new Node(data, head);
         head = node;
         size++;
+    }
+    //view() method to view the complete contact list.
+    @Override
+    public void view() {
+        try{
+            if (head != null) {
+                Node temp1 = head;
+                ArrayList<String> listNames = new ArrayList<>(size);
+                String strName = "";
+                while (temp1 != null) {
+                    strName = (String) (temp1.getData().getFName() + temp1.getData().getLName());
+                    listNames.add(strName);
+                    temp1 = temp1.getNext();
+                }
+                Collections.sort(listNames);
+                System.out.println("---Here are all your contacts---");
+                for (int i = 0; i < listNames.size(); i++) {
+                    Node temp = head;
+                    String str = "";
+                    while (temp != null) {
+                        str = (String) (temp.getData().getFName() + temp.getData().getLName());
+                        if (listNames.get(i).equals(str)) {
+                            System.out.println("-------- * -------- * -------- * --------");
+                            System.out.println(temp.getData());
+                            System.out.println("-------- * -------- * -------- * --------");
+                            break;
+                        }
+                        temp = temp.getNext();
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("Contact List is Empty!");
+        }
     }
 }
